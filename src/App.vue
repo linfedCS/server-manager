0cdf73957f00207278b0246d99f0cf21ef7cbd5e
Kirill Galimov <galimovdev@gmail.com>
2025-09-10 16:22:19 +0300

    feat: improve ts config and add prettier

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <main class="main">
-    <div class="container">
-      <AppHeading :level="2" class="main__heading" has-line uppercase>
-        Сервера
-      </AppHeading>
+    <main class="main">
+        <div class="container">
+            <AppHeading
+                :level="2"
+                class="main__heading"
+                has-line
+                uppercase
+            >
+                Сервера
+            </AppHeading>
 
-      <div class="main__servers">
-        <ServerCard />
-        <ServerCard />
-        <ServerCard />
-      </div>
-    </div>
-  </main>
+            <div class="main__servers">
+                <ServerCard />
+                <ServerCard />
+                <ServerCard />
+            </div>
+        </div>
+    </main>
 </template>
 
 <script setup lang="ts">
-  import AppHeading from "./components/shared/AppHeading.vue";
-  import ServerCard from "./components/ServerCard/index.vue";
+import AppHeading from './components/shared/AppHeading.vue'
+import ServerCard from './components/ServerCard/index.vue'
 </script>
 
 <style lang="scss">
-  .main {
+.main {
     min-height: 100vh;
     display: flex;
     flex-direction: column;
@@ -28,14 +33,14 @@
     justify-content: center;
 
     &__heading {
-      width: 100%;
+        width: 100%;
     }
 
     &__servers {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      gap: 20px;
-      margin-top: 40px;
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 20px;
+        margin-top: 40px;
     }
-  }
+}
 </style>
--- src/components/shared/AppHeading.vue
+++ src/components/shared/AppHeading.vue
@@ -1,23 +1,23 @@
 <template>
-  <component
-    :is="`h${level}`"
-    class="app-heading"
-    :class="[`level-${level}`, { 'has-line': hasLine, uppercase }]"
-  >
-    <slot />
-  </component>
+    <component
+        :is="`h${level}`"
+        class="app-heading"
+        :class="[`level-${level}`, { 'has-line': hasLine, uppercase }]"
+    >
+        <slot />
+    </component>
 </template>
 
 <script setup lang="ts">
-  const { level, hasLine, uppercase } = defineProps<{
-    level: 1 | 2 | 3 | 4 | 5 | 6;
-    hasLine?: boolean;
-    uppercase?: boolean;
-  }>();
+const { level, hasLine, uppercase } = defineProps<{
+    level: 1 | 2 | 3 | 4 | 5 | 6
+    hasLine?: boolean
+    uppercase?: boolean
+}>()
 </script>
 
 <style lang="scss">
-  .app-heading {
+.app-heading {
     font-weight: 600;
     display: inline-flex;
     flex-direction: column;
@@ -26,51 +26,51 @@
     margin: 0;
 
     &.uppercase {
-      text-transform: uppercase;
+        text-transform: uppercase;
     }
 
     &.has-line {
-      margin-bottom: 20px;
+        margin-bottom: 20px;
 
-      &::after {
-        content: "";
-        display: block;
-        position: absolute;
-        bottom: -10px;
-        left: 0;
-        right: 0;
-        width: 60px;
-        height: 4px;
-        background-color: #ff3838;
-        margin-left: auto;
-        margin-right: auto;
-      }
+        &::after {
+            content: '';
+            display: block;
+            position: absolute;
+            bottom: -10px;
+            left: 0;
+            right: 0;
+            width: 60px;
+            height: 4px;
+            background-color: #ff3838;
+            margin-left: auto;
+            margin-right: auto;
+        }
     }
 
     &.level-1 {
-      font-size: 40px;
-      letter-spacing: 0.05em;
+        font-size: 40px;
+        letter-spacing: 0.05em;
     }
 
     &.level-2 {
-      font-size: 32px;
-      letter-spacing: 0.05em;
+        font-size: 32px;
+        letter-spacing: 0.05em;
     }
 
     &.level-3 {
-      font-size: 24px;
+        font-size: 24px;
     }
 
     &.level-4 {
-      font-size: 20px;
+        font-size: 20px;
     }
 
     &.level-5 {
-      font-size: 18px;
+        font-size: 18px;
     }
 
     &.level-6 {
-      font-size: 16px;
+        font-size: 16px;
     }
-  }
+}
 </style>
